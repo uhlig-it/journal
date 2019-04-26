b693b43005c4372e067655d2442cb519a1435781
Steffen Uhlig <steffen@familie-uhlig.net>
2019-04-26 17:51:37 +0200

    Rubocoppin'

--- db/migrations/001_create_journal.rb
+++ db/migrations/001_create_journal.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 Sequel.migration do
   change do
     create_table(:entries) do
       primary_key :id
       DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP, index: true
       String :text, null: false
-      constraint(:text_min_length) { Sequel.char_length(text) > 0 }
+      constraint(:text_min_length) { Sequel.char_length(text).positive? }
     end
   end
 end
