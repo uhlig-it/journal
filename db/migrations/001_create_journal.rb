8b323ed103b224bfceea8100172048fe22b7a091
Steffen Uhlig <steffen@familie-uhlig.net>
2019-04-26 18:58:20 +0200

    Fix broken fix
    Rubocop suggestion was wrong.

--- db/migrations/001_create_journal.rb
+++ db/migrations/001_create_journal.rb
@@ -6,7 +6,9 @@ Sequel.migration do
       primary_key :id
       DateTime :created_at, default: Sequel::CURRENT_TIMESTAMP, index: true
       String :text, null: false
-      constraint(:text_min_length) { Sequel.char_length(text).positive? }
+      # rubocop:disable Style/NumericPredicate
+      constraint(:text_min_length) { Sequel.char_length(text) > 0 }
+      # rubocop:enable Style/NumericPredicate
     end
   end
 end
